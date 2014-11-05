2ddd23d6cbc07de6e0d6d38d1c9734d857209a74
Administrator <admin@local.host>
2014-11-05 08:14:50 +0800

    FEATURE - Merge author link with name in email

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,14 @@ class Author < ActiveRecord::Base
 
   has_many :articles
   has_many :posts
+
+  def author_home_link
+    if uri = URI(self.link)
+      link = self.posts.first.post_link
+      uri_tmp = URI(link)
+      uri_tmp.scheme + "://" + uri_tmp.host + self.link
+    else
+      self.link
+    end
+  end
 end
